2e7ffbea5adddfd6633886c3e3af0d778ca9ea70
jas-pabla <102491970+jas-pabla@users.noreply.github.com>
2022-03-31 16:23:01 -0400

    Create block_j.cc

---
+++ files/block_j.cc
@@ -0,0 +1,39 @@
+#include "jBlock.h"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> rotate1 { // no rotation
+        "....",
+        "....",
+        "J...",
+        "JJJ."
+};
+
+vector<string> rotate2 { // rotate 90 degrees clockwise
+        "....",
+        "JJ..",
+        "J...",
+        "J..."
+};
+
+vector<string> rotate3 { // rotate 180 degrees clockwise
+        "....",
+        "....",
+        "JJJ.",
+        "..J."
+};
+
+vector <string> rotate4 { // rotate 270 degrees clockwise
+        "....",
+        ".J..",
+        ".J..",
+        "JJ.."
+};
+
+const vector<vector<string>> jBlock::jBlockRotations = {rotate1, rotate2, rotate3, rotate4};
+
+jBlock::jBlock(bool heavy_effect) : Block(0, 0, 0, heavy_effect, false) {}
+
+char jblock::get
