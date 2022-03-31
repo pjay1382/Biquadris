af95e9de9e5770158c890b75ad4e2f6e884aa524
pjay1382 <93274983+pjay1382@users.noreply.github.com>
2022-03-31 17:36:57 -0400

    Update block_lvl4.cc

--- files/block_lvl4.cc
+++ files/block_lvl4.cc
@@ -18,6 +18,6 @@ lvl4Block::lvl4Block() : Block(5, 0, 0, false, false) {}
 
 char lvl4Block::get_block_type() { return '*'; }
 
-vector<vector<string>> jBlock::get_block() { return jBlockRotations; }
+vector<vector<string>> lvl4Block::get_block() { return lvl4BlockRotations; }
 
 lvl4Block::~lvl4Block() {}
