c80d1080f4c0e8f59b7a15fff6ae6cdf62c46ef6
jas-pabla <102491970+jas-pabla@users.noreply.github.com>
2022-03-31 17:14:34 -0400

    Create block_lvl4.cc

---
+++ files/block_lvl4.cc
@@ -0,0 +1,23 @@
+#include "block.h"
+#include "block_lvl4.h"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> rotate1 {
+        "....",
+        "....",
+        "....",
+        "*..."
+};
+
+vector<vector<string>> lvl4Block::lvl4BlockRotations = {rotate1, rotate1, rotate1, rotate1};
+
+lvl4Block::lvl4Block(bool heavy_effect) : Block(5, 0, 0, false, heavy_effect) {}
+
+char lvl4Block::get_block_type() { return '*'; }
+
+vector<vector<string>> jBlock::get_block() { return jBlockRotations; }
+
+lvl4Block::~lvl4Block() {}
