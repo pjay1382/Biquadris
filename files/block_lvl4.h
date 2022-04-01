81f8e66a4a05b86676c762f689f7eab91c480ad1
pjay1382 <93274983+pjay1382@users.noreply.github.com>
2022-04-01 17:31:50 -0400

    Update block_lvl4.h

--- files/block_lvl4.h
+++ files/block_lvl4.h
@@ -8,7 +8,7 @@ class lvl4Block : public Block {
         static std::vector<std::vector<std::string>> lvl4BlockRotations;
 
         public:
-                lvl4Block(bool heavy_effect);
+                lvl4Block();
                 virtual char get_block_type() override;
                 virtual std::vector<std::vector<std::string>> get_block() override;
                 virtual ~lvl4Block() override;
