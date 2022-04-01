8ed9f667a9d4d5ad47f57126ae35c6f41b9cbf26
pjay1382 <93274983+pjay1382@users.noreply.github.com>
2022-04-01 17:18:51 -0400

    Update block_t.cc

--- files/block_t.cc
+++ files/block_t.cc
@@ -37,7 +37,7 @@ vector<vector<string>> tBlock::tBlockRotations = {rotate1, rotate2, rotate3, rot
 
 tBlock::tBlock(bool heavy_effect) : Block(0, 0, 0, false, heavy_effect) {}
 
-char tblock::get_block_type() { return 'T'; }
+char tBlock::get_block_type() { return 'T'; }
 
 vector<vector<string>> tBlock::get_block() { return tBlockRotations; }
 
