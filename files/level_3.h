1f93ba30c88edc386995e0743c987b8491f3ef47
pjay1382 <93274983+pjay1382@users.noreply.github.com>
2022-04-02 14:36:23 -0400

    Update level_3.h

--- files/level_3.h
+++ files/level_3.h
@@ -18,10 +18,10 @@ class Level3 : public Level {
         Level3(bool random, std::string file_name = "");
         ~Level3() override;
         Block *next_block(int index) override;
-        int get_lvl() override;
-        bool is_random() override;
-        std::string get_file() override;
-        void get_block() override;
+        int getlvl() override;
+        bool israndom() override;
+        std::string getfile() override;
+        void getblock() override;
 };
 
 #endif
