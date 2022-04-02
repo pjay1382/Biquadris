6b3f0ea87bdb6fc0ee40db1e37978de6d1fe698b
pjay1382 <93274983+pjay1382@users.noreply.github.com>
2022-04-02 14:46:36 -0400

    Update level_1.cc

--- files/level_1.cc
+++ files/level_1.cc
@@ -59,7 +59,7 @@ void Level1::getblock(){
         inp.open(file_name);
         inp>>block_piece;
         while(true) {
-            if(block_piece != nullptr) blocks.emplace_back(block_piece);
+            if(block_piece != "") blocks.emplace_back(block_piece);
             else break;
             inp>>block_piece;
         }
